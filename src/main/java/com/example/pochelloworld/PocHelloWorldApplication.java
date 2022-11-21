1812aafba45d374b47acbcc24fb677cc8374d60d
Luan Beck <luanbeck@gmail.com>
2022-11-21 11:42:17 -0300

    first commit

---
+++ src/main/java/com/example/pochelloworld/PocHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.pochelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocHelloWorldApplication.class, args);
+	}
+
+}
